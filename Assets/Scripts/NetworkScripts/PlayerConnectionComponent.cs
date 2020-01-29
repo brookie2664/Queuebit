1fa806e64964b1bed623cfe5d963e7adb1aed5f0
Lenno Liu <lenno.liu@gmail.com>
2020-01-29 10:30:19 -0500

    Initial commit

---
+++ Assets/Scripts/NetworkScripts/PlayerConnectionComponent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionComponent : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerConnectionObject is an always-on, invisible object that is spawned when a player connects to the game.
+        //
+        if (isLocalPlayer == false) {
+            //object belongs to another player
+            return;
+        }
+
+        Debug.Log("Spawning Player Unit");
+
+        //Instantiate() only creates an object on the LOCAL COMPUTER
+        //Even if it has a networkidentity, it will still not exist on the network (and therefore not on any other client)
+        //Unless you use NetworkServer.Spawn().
+        //NetworkServer.Spawn(PlayerUnitPrefab);
+        CmdSpawnPlayerQbit();
+        //Visual effects can be instantiated on local computer - that's fine.
+
+    }
+
+    public GameObject PlayerUnitPrefab;
+    public string PlayerName = "Anon";
+    // Update is called once per frame
+    void Update()
+    {
+        //Update runs on everyone's computer, wether or not they own this particular player object
+        if (isLocalPlayer == false) {
+            return;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.P)) {
+            CmdSpawnPlayerQbit();
+        }
+
+
+    }
+
+    //Commands:
+    //Commands are special function that only get executed on the server
+
+    [Command]
+    void CmdSpawnPlayerQbit() {
+        //Guaranteed to be on the server right now
+        //Can only call command spawn for things you have authority over.
+        GameObject go = Instantiate(PlayerUnitPrefab);
+
+
+        //go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+        //Now, the game object exists on the server
+        //Propagate it to all clients (and also wire up the NetworkIdentity)
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Note: Update() here runs on all playerunits.
+        if (hasAuthority == false) { //Do you have authority over this object?
+            return;
+        }
+        
+        //if IsActive:
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Spacebar special
+        } 
+        else {
+            if (Input.GetKeyDown(KeyCode.W)) {
+                this.transform.Translate(0, 0, 1);
+            }
+            if (Input.GetKeyDown(KeyCode.A)) {
+                this.transform.Translate(-1, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S)) {
+                this.transform.Translate(0, 0, -1);
+            }
+            if (Input.GetKeyDown(KeyCode.D)) {
+                this.transform.Translate(1, 0, 0);
+            }
+
+        }
+
+        //TODO: Decide if we want this self-destruct code
+        if(Input.GetKeyDown(KeyCode.Backspace)) {
+            Destroy(gameObject);
+        }
+
+    }
+}
