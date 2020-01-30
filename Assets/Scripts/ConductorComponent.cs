2524239184ae42ce95d4ac4118a92cd63ecadcd3
Lenno Liu <lenno.liu@gmail.com>
2020-01-30 12:00:20 -0500

    1-30 Start
    Things I'm working on:
    Conductor (music/gameplay controller)
    Camera (for each player)
    
    Added:
    Beat test sample track

---
+++ Assets/Scripts/ConductorComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConductorComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float songBpm;
+    public float secPerBeat;
+    public float songPosition;
+    public float songPositionInBeats;
+    public float dspSongTime;
+    public AudioSource musicSource;
+
+
+    void Start()
+    {
+        //Load AudioSource attached to conductor gameobject:
+        musicSource = GetComponent<AudioSource>();
+        secPerBeat = 60f / songBpm;
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+    }
+}
