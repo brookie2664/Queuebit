b9dfa8194151d1e4ee1972a8e3c82f96d0f6b586
Mitchell Philipp <mgpalpha1@gmail.com>
2020-02-29 22:24:04 -0500

    fixed camera smoothing

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class PlayerCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform target;
+    public Vector3 target;
     public float smoothTime = 0.2f;
-    public Vector3 offset = new Vector3(0, 10, 0);
+    public Vector3 offset = new Vector3(0, 0, -1);
     private Vector3 velocity = Vector3.zero;
     
     void FixedUpdate() {
         if (target != null) {
-            Vector3 desiredPosition = target.position + offset;
+            Vector3 desiredPosition = target + offset;
             Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
             transform.position = smoothedPosition;
         }
     }
 
-    public void setCameraTarget(Transform inputTarget) {
+    public void setCameraTarget(Vector3 inputTarget) {
         target = inputTarget;
     }
 
