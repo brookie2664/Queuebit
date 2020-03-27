e38f860174d1716a7d06410874dcd9895303e921
Lenno Liu <lenno.liu@gmail.com>
2020-03-27 16:24:32 -0400

    Working synced SFX

--- Assets/Scripts/NetworkScripts/PlayerConnectionComponent.cs
+++ Assets/Scripts/NetworkScripts/PlayerConnectionComponent.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(AudioSource))]
 public class PlayerConnectionComponent : NetworkBehaviour
 {
 
+    private AudioSource source;
+    public AudioClip[] clips;
+    public bool loopingAudio = false;
+    public float bpm = 120;
+    public double localBGMStartTime;
     public GameObject gameStateObject;
     private GameState gameState;
 
@@ -29,6 +35,32 @@ public class PlayerConnectionComponent : NetworkBehaviour
         playerCamera.GetComponent<PlayerCameraController>().setCameraTarget(targetCell.transform.position);
     }
 
+    //Clips reference:
+    //0: Movement SFX
+    //1: 
+    [ClientRpc]
+    public void RpcPlayOneShotOnClients(int id) {
+        //if(isLocalPlayer) {
+            source.PlayOneShot(clips[id]);
+        //}
+    }
+    // [ClientRpc]
+    // void RpcPlayClipAtPointOnClients(int id, Vector3 soundPosition) {
+    //     GameState.source.PlayClipAtPoint(clips[id], soundPosition);
+    //     //AudioClip[] must be the same on all clients.
+    // }
+    [ClientRpc]
+    public void RpcSendBGMStartToClients(bool loopingAudio, int id) {
+        //Starts audio
+        if (loopingAudio == true) {
+            //Do loop logic here?
+        } 
+        else {
+            source.PlayOneShot(clips[id]);
+        }
+        localBGMStartTime = AudioSettings.dspTime;
+    }
+
     [Command]
     public void CmdCreatePlayer(NetworkIdentity playerId) {
         gameState.CreatePlayer(playerId);
@@ -44,6 +76,7 @@ public class PlayerConnectionComponent : NetworkBehaviour
     {
         gameStateObject = GameObject.Find("GameState");
         gameState = gameStateObject.GetComponent<GameState>();
+        source = this.GetComponent<AudioSource>();
 
         // PlayerConnectionObject is an always-on, invisible object that is spawned when a player connects to the game.
         //
@@ -58,7 +91,7 @@ public class PlayerConnectionComponent : NetworkBehaviour
         Debug.Log("Spawning Player Unit");
 
         CmdCreatePlayer(GetComponent<NetworkIdentity>());
-
+        
     }
 
     public string PlayerName = "Anon";
