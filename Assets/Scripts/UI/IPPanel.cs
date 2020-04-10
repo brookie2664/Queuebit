65b7b85af14acbcaed61916069f2329bc1e93974
Mitchell Philipp <mgpalpha1@gmail.com>
2020-04-10 11:50:25 -0400

    Ip shows on screen while connecting

---
+++ Assets/Scripts/UI/IPPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class IPPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Text ipText;
+    [SerializeField]
+    private GameState gameState;
+
+    private bool show = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string hostName = System.Net.Dns.GetHostName();
+        string localIP = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
+        ipText.text = localIP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (show && gameState.IsGameLoaded()) {
+            show = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
